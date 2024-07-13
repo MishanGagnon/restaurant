06439324711418a223b55805ea737b0716fd9d80
Mishan Gagnon <mishangagnon@gmail.com>
2024-07-13 15:29:13 -0700

    pracitce page

---
+++ app/practice/TodoList.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+const TodoList =() => {
+    return <h1>Todo List</h1>
+}
+
+export default TodoList
+
---
+++ app/practice/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import TodoList from './TodoList'
+
+
+
+const Page = () => {
+    return (
+        <div>
+            <TodoList />
+        </div>
+    )
+}
+export default Page
