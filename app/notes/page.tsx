4edcbe0ab3819f0df3fa61c033ec06ff8dec5e03
Mishan Gagnon <mishangagnon@gmail.com>
2024-07-13 12:10:37 -0700

    hi alert

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -15,7 +15,7 @@ const Notes = () => {
     
     return (
         <div>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">hi</button>
+            <button  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={()=>{window.alert('hi')}}>hi</button>
         </div>
     )
 }
