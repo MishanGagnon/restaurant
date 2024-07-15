2af1c8d303da45b09229438ebb3f6617bf008b78
Mishan Gagnon <mishangagnon@gmail.com>
2024-07-14 18:04:02 -0700

    adding name selection at lobbyid page

--- app/lobby/[lobbyId]/page.tsx
+++ app/lobby/[lobbyId]/page.tsx
@@ -21,9 +21,16 @@ const Lobby = () => {
   const paramName = searchParams.get('name');
   const [players, setPlayers] = useState<Player[]>([]);
   const [name, setName] = useState(paramName ?? '');
+  const [nameInput, setNameInput] = useState<string>('')
+  const submitName = () => {
+    setName(()=>nameInput)
+    const url = new URL(window.location.href);
+    url.searchParams.set('name', nameInput);
+    router.replace(url.toString(), undefined,)
+  }
+
   useEffect(() => {
     if (!name) {
-      router.push('/lobby');
       return;
     }
 
@@ -42,9 +49,32 @@ const Lobby = () => {
     return () => {
       socket.disconnect();
     };
-  }, [lobbyId, name, router]);
+  }, [lobbyId,name]);
 
   return (
+    name == '' ? 
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-2xl font-bold mb-4 text-black">Choose display name</h1>
+      <div>
+      <input
+        type="text"
+        value={nameInput}
+        className="m-5 p-2 text-black border border-gray-300 rounded"
+        onChange={(e) => setNameInput(e.target.value)}
+        placeholder="Set Name"
+      />
+      <button
+        onClick={submitName}
+        className="m-5 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+      >
+        Enter
+      </button>
+
+      </div>
+    </div>
+
+    :
+
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-black">
       <h1 className="text-2xl font-bold mb-4 text-black">Lobby: {lobbyId}</h1>
       <h2 className="text-xl mb-4 text-black">Name: {name}</h2>
