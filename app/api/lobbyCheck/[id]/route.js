6f208cb13542a7a31c11afa044cbe32d5d054f75
Mishan Gagnon <mishangagnon@gmail.com>
2024-07-27 21:36:50 -0700

    removes lobby if no users join

--- app/api/lobbyCheck/[id]/route.js
+++ app/api/lobbyCheck/[id]/route.js
@@ -5,6 +5,5 @@ import {
 
 export async function GET(request, { params }) {
   const { id } = params;
-  console.log(id in getActiveRooms())
   return NextResponse.json({isActiveLobby : (id in getActiveRooms()) });
 }
