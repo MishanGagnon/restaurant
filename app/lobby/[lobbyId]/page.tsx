b5c96ba65cdade390b765244cd90f125b3ac3076
Mishan Gagnon <mishangagnon@gmail.com>
2024-07-28 11:20:24 -0700

    host start game button

--- app/lobby/[lobbyId]/page.tsx
+++ app/lobby/[lobbyId]/page.tsx
@@ -24,23 +24,21 @@ const Lobby = () => {
   const [nameInput, setNameInput] = useState<string>('');
   const [loading, setLoading] = useState(true);
   const [socketId, setSocketId] = useState<string>('');
-  const [isHost, setHost] = useState<boolean>(false)
-  const [hostPlayer, setHostPlayer] =  useState<Player>();
-  
+  const [isHost, setIsHost] = useState<boolean>(false);
+  const [hostPlayer, setHostPlayer] = useState<Player>();
+
   const submitName = () => {
     setName(nameInput);
     const url = new URL(window.location.href);
     url.searchParams.set('name', nameInput);
     router.replace(url.toString(), undefined);
   };
-  
+
   const checkValidLobby = async () => {
     try {
       const response = await fetch(`http://localhost:3000/api/lobbyCheck/${lobbyId}`);
-      
       if (response.ok) {
         const data = await response.json();
-        
         if (data.isActiveLobby) {
           return true;
         }
@@ -55,20 +53,18 @@ const Lobby = () => {
     const validateLobbyAndJoin = async () => {
       console.log('re-render');
       const isValid = await checkValidLobby();
-
       if (!isValid) {
         router.push('/lobby');
         return;
       }
       setLoading(false);
-    } 
+    };
 
     validateLobbyAndJoin();
     if (name) {
       socket = io();
 
       socket.on('connect', () => {
-        
         setSocketId(socket.id as string);
         if (lobbyId) {
           console.log('joining as new player');
@@ -78,11 +74,12 @@ const Lobby = () => {
 
       socket.on('lobbyPlayerList', (players: Player[]) => {
         setPlayers(() => players);
-        console.log(players)
-        let hostPlayer = players.find((player) => player.host == true)
-        setHostPlayer(hostPlayer)
-        if(hostPlayer && hostPlayer.id == socket.id ){
-          setHost(()=> true);
+        console.log(players);
+        const hostPlayer = players.find((player) => player.host === true);
+        setHostPlayer(hostPlayer);
+        if (hostPlayer && hostPlayer.id === socket.id) {
+          console.log('am host');
+          setIsHost(true);
         }
         console.log('players updated');
       });
@@ -144,9 +141,15 @@ const Lobby = () => {
       <h1 className="text-2xl font-bold mb-4 text-black">Lobby: {lobbyId}</h1>
       <h2 className="text-xl mb-4 text-black">Name: {name}</h2>
       <h3 className="text-lg font-semibold mb-2 text-black">Active Players:</h3>
-      <div className='flex flex-col'>
+      {isHost && <button
+            // onClick={}
+            className="m-5 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
+            Start Game
+          </button>}
+      <div className="flex flex-col">
         {players.map(player => (
-          <PlayerCard key={player.id} isHost={player.id == hostPlayer.id} name={player.name ?? 'nameError'} />
+          <PlayerCard key={player.id} isHost={player.id === hostPlayer?.id} name={player.name ?? 'nameError'} />
         ))}
       </div>
     </div>
