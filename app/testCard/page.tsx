68f708a11ec269aa6654f854dbd2ecc0af8252f1
Mishan Gagnon <mishangagnon@gmail.com>
2024-08-21 14:13:10 -0700

    build fixes

--- app/testCard/page.tsx
+++ app/testCard/page.tsx
@@ -1,19 +1,38 @@
 'use client'
 
-import React from 'react'
-import data from '../../components/restaurantTestData'
-import TestPage from '../../components/RenderPage'
+import React, { useEffect, useState } from 'react';
+import data from '../../components/restaurantTestData';
+import TestPage from '../../components/RenderPage';
 import io, { Socket } from 'socket.io-client';
 
-let socket : Socket;
+let socket: Socket;
 
-const page = () => {
-    socket = io();
+const TestCard = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);  // Mark component as mounted
+
+        if (isMounted) {
+            // Any code that requires `window` or `HTMLElement`
+            socket = io();
+        }
+
+        // Cleanup function
+        return () => {
+            setIsMounted(false); // Mark component as unmounted
+        };
+    }, [isMounted]);
+
+    if (!isMounted) {
+        return null; // Return null or a loading indicator if not mounted
+    }
+
     return (
         <div>
             <TestPage socket={socket} restaurants={data} lobbyId='YUHH' playerId='asdgy12370123bqwd' />
         </div>
-    )
+    );
 }
 
-export default page
+export default TestCard;
